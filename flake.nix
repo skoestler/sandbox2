41b2a3eeefc5ed266ec7ed3d30ffd64300329b9f
Shayne Koestler <shayne.koestler+git@gmail.com>
2026-03-19 09:33:47 -0500

    initial bun commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+    description = "A simple distributed development environment built around NATS and etcd";
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+    outputs = { self, nixpkgs }:
+    let
+        system = "aarch64-darwin"; # Adjust to "x86_64-linux" or "aarch64-linux" as needed
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+    in {
+        devShells.${system}.default = pkgs.mkShell {
+            packages = [
+              pkgs.bun
+              pkgs.etcd
+              pkgs.nats-server
+              pkgs.postgresql
+              pkgs.process-compose
+              pkgs.httpie
+            ];
+
+            shellHook = ''
+                export PGHOST=$PWD/data/postgres/locks
+                export ETCD_DATA_DIR=$PWD/data/etcd
+            '';
+        };
+    };
+}
